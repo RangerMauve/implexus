6c0ce21ded773f58dc5da5c7d94dc3611477e2bf
RangerMauve <RangerMauve@htomail.com>
2015-01-18 17:20:48 -0500

    Initial flesh out of the library

---
+++ index.js
@@ -0,0 +1,65 @@
+var map = require("map-async");
+var par = require("par");
+
+exports.build = function build(modules, graph, cb) {
+	build_streams(modules, graph, function(err, stream_map) {
+		if (err) return cb(err);
+		try {
+			link_streams(graph, stream_map)
+		} catch (e) {
+			cb(e);
+			return;
+		}
+		cb(null, stream_map);
+	});
+}
+
+function link_streams(graph, stream_map) {
+	Object.keys(streams)
+		.forEach(par(link_successors, graph, stream_map));
+
+	return stream_map;
+}
+
+function build_streams(modules, graph, cb) {
+	var node_map = build_node_map(graph);
+	map(node_map, function(node, name, cb) {
+		build_stream(modules, name, node, cb);
+	}, cb);
+}
+
+function build_stream(modules, name, node, cb) {
+	var type = node.type;
+	var builder = modules[type];
+	if (!builder)
+		return cb(new Error("Stream type " + type + " does not exist for" + name), null);
+	try {
+		builder(name, node, cb);
+	} catch (e) {
+		cb(e);
+	}
+}
+
+function build_node_map(graph) {
+	var nodes = graph.nodes();
+	return nodes.reduce(function(build, name) {
+		build[name] = graph.node(name);
+		return build;
+	}, {});
+}
+
+function link_successors(graph, stream_map, from) {
+	var outbound = graph.successors(from);
+	var from_stream = stream_map[from];
+	outbound
+		.map(par(get_stream, stream_map))
+		.forEach(par(pipe_together, from));
+}
+
+function pipe_together(from, to) {
+	from.pipe(to);
+}
+
+function get_stream(stream_map, name) {
+	return (stream_map[name])
+}
