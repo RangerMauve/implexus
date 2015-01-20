9051e5250b2c6b30943f5734876af5278413b426
RangerMauve <RangerMauve@htomail.com>
2015-01-19 23:57:56 -0500

    Fixed example to work with new api

--- example/index.js
+++ example/index.js
@@ -1,18 +1,22 @@
-var implexus = require("../");
-var dot = require("graphlib-dot");
+var Implexus = require("../").Implexus;
 var fs = require("fs");
 var util = require("util");
 
-var modules = {
-	array: function(node, cb) {
-		var but = require("but");
-		var streamArray = require("stream-array");
-		var source = (node.list || "")
-			.split(",")
-			.map(but(parseInt));
-		var stream = streamArray(source);
-		cb(null, stream);
-	},
+var graph = fs.readFileSync(__dirname + "/graph.dot", "utf8");
+
+var implexus = new Implexus();
+
+implexus.define("array",function(node, cb) {
+	var but = require("but");
+	var streamArray = require("stream-array");
+	var source = (node.list || "")
+	.split(",")
+	.map(but(parseInt));
+	var stream = streamArray(source);
+	cb(null, stream);
+});
+
+implexus.define({
 	stdout: function(node, cb) {
 		var stdout = require("stdout");
 		var prefix = node.prefix || "";
@@ -25,14 +29,12 @@ var modules = {
 			return number + amount;
 		}));
 	}
-};
+});
 
-var graph_src = fs.readFileSync(__dirname + "/graph.dot", "utf8");
-var graph = dot.read(graph_src);
-
-implexus.build(modules, graph, function(err, streams) {
-	if (err) console.log(err, err.stack);
-	else console.log("Stream map:", util.inspect(streams, {
+implexus.build(graph, function(err, streams) {
+	var opts = {
 		depth: 0
-	}));
+	};
+	if (err) console.log(err, err.stack);
+	else console.log("Stream map:", util.inspect(streams, opts));
 });
